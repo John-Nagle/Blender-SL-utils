2638d079bc5456fe088be638a3e3e7808dd19ccc
John Nagle <nagle@sitetruth.com>
2018-12-19 11:49:16 -0800

    Cleanup. Now needs a dialog

--- linearstretch.py
+++ linearstretch.py
@@ -1,18 +1,24 @@
 #
-#   Escalator rescaling assist
+#   linearstretch.py
+#
+#   Linear stretch along a vector defined by two reference points.
 #
 #   Animats
 #   December, 2018
-#
-#
+#   License: GPL
 #
 #   This stretches a model by moving the top vertex set
 #   along the vector from the bottom ref point to the
 #   top ref point.
 #
-#   This is used to adjust the length of our escalator.
+#   Used to adjust the length of our escalator.
 #   The model is designed so that the section stretched
 #   has no vertices within it.
+#
+#   Not an addon. Typically run by
+# 
+#   filename = "linearstretch.py"
+#   exec(compile(open(filename).read(), filename, 'exec'))
 #
 import bpy
 import math
@@ -27,7 +33,7 @@ REFTOP = "Top ref"
 #
 def getvertsingroup(obj, groupobj) :
     groupix = groupobj.index                    # group index
-    print("Verts of index %d, group %s, object %s" % (groupix, groupobj.name, obj.name)) # ***TEMP***
+    ####print("Verts of index %d, group %s, object %s" % (groupix, groupobj.name, obj.name)) # ***TEMP***
     verts = []                                  # array of verts
     for v in obj.data.vertices :                # search for vert in group
         for g in v.groups :
@@ -39,7 +45,6 @@ def getvertsingroup(obj, groupobj) :
 #   stretchmodel -- stretch selected model appropriately
 #
 def stretchmodel(bottomrefname, toprefname, topname, dist) :
-    print("Stretchmodel")
     #   Sanity checks before starting
     context = C
     if not context.selected_objects :
@@ -58,33 +63,24 @@ def stretchmodel(bottomrefname, toprefname, topname, dist) :
     bottomrefvs = getvertsingroup(target, bottomrefgroup)   # verts in top ref group
     toprefvs = getvertsingroup(target, toprefgroup)         # verts in bottom ref group
     topvs = getvertsingroup(target, topgroup)               # verts to move
-    print("Top ref group: %d verts. Bottom ref group: %d verts. Verts to move: %d." % (len(toprefvs), len(bottomrefvs), len(topvs))) # ***TEMP***
+    ####print("Top ref group: %d verts. Bottom ref group: %d verts. Verts to move: %d." % (len(toprefvs), len(bottomrefvs), len(topvs))) # ***TEMP***
     if len(toprefvs) != 1 : 
         return({'ERROR_INVALID_INPUT'}, "Reference vertex group \"%s\" had %d vertices, not one." % (toprefgroup.name,len(toprefvs)))
     if len(bottomrefvs) != 1 : 
         return({'ERROR_INVALID_INPUT'}, "Reference vertex group \"%s\" had %d vertices, not one." % (bottomrefgroup.name,len(bottomrefvs)))
-    toprefv = toprefvs[0]
-    bottomrefv = bottomrefvs[0]
-    refvec = [toprefv.co[0] - bottomrefv.co[0], toprefv.co[1] - bottomrefv.co[1], toprefv.co[2] - bottomrefv.co[2]] # movement vec
-    print("topref: %s  bottomref: %s  refvec: %s" % (toprefv.co, bottomrefv.co, refvec))
-    refvecl = math.sqrt(refvec[0]*refvec[0] + refvec[1]*refvec[1] + refvec[2]*refvec[2])
-    if refvecl < 0.001 :
+    refvec = toprefvs[0].co - bottomrefvs[0].co             # movement direction 
+    print("topref: %s  bottomref: %s  refvec: %s" % (toprefvs[0].co, bottomrefvs[0].co, refvec))    # ***TEMP***
+    if refvec.magnitude < 0.001 :
         return({'ERROR_INVALID_INPUT'}, "Reference vertices are in the same place.")
-    refvecnorm = [refvec[0]/refvecl, refvec[1]/refvecl, refvec[2]/refvecl]              # normalize
-    ####return # ***TEMP***
+    refvecnorm = refvec.normalized()                        # unit vector
+    #   All checks passed. OK to perform stretch.
     #   Move verts
     for v in topvs :
-        v.co.x = v.co.x + refvecnorm[0] * dist
-        v.co.y = v.co.y + refvecnorm[1] * dist
-        v.co.z = v.co.z + refvecnorm[2] * dist
-        
-    
-
-        
-    return None                                 # success
+        v.co = v.co + refvecnorm * dist
+    return None                                             # success
 
 
 def test() :
-    msg = stretchmodel(REFBOTTOM, REFTOP, VERTSTOP, 500.0)
+    msg = stretchmodel(REFBOTTOM, REFTOP, VERTSTOP, 5.0)
     if (msg) :
         print("Error: %s" % (str(msg),))
