589769d36fba6c256fdc92cd4267035b8a94d9c2
John Nagle <nagle@sitetruth.com>
2018-12-19 12:51:42 -0800

    Added dialog

--- linearstretch.py
+++ linearstretch.py
@@ -44,12 +44,8 @@ def getvertsingroup(obj, groupobj) :
 #
 #   stretchmodel -- stretch selected model appropriately
 #
-def stretchmodel(bottomrefname, toprefname, topname, dist) :
+def stretchmodel(target, bottomrefname, toprefname, topname, dist) :
     #   Sanity checks before starting
-    context = C
-    if not context.selected_objects :
-        return({'ERROR_INVALID_INPUT'}, "Nothing selected.")
-    target = context.selected_objects[-1]       # target impostor (last selection)
     if target.type != 'MESH' :
         return({'ERROR_INVALID_INPUT'}, "Selected object \"%s\" must be a mesh." % (target.name,))
     if target.scale[0] < 0 or target.scale[1] < 0 or target.scale[2] < 0 :
@@ -78,9 +74,52 @@ def stretchmodel(bottomrefname, toprefname, topname, dist) :
     for v in topvs :
         v.co = v.co + refvecnorm * dist
     return None                                             # success
+    
+#
+#   asksize -- pop up dialog for size
+#
+class AskSizeDialogOperator(bpy.types.Operator):
+    bl_idname = "object.ask_size_dialog_operator"
+    bl_label = "Resizing by stretching"
+
+    desired_height = bpy.props.FloatProperty(name="Desired height")
+    
+    def execute(self, context) :
+        result = self.run(context)
+        if result :                                 # report fail
+            self.report(result[0], result[1])
+            return {'CANCELLED'}                    # return fail code       
+        return {'FINISHED'}
 
 
+    def run(self, context):
+        print("Dialog result: %1.2f" % (self.desired_height,))
+        if self.desired_height < 0.1 or self.desired_height > 64.0 :
+            return({'ERROR_INVALID_INPUT'}, "Desired height %1.3f out of range." % (self.desired_height))      
+        if not context.selected_objects :
+            return({'ERROR_INVALID_INPUT'}, "Nothing selected.")
+        target = context.selected_objects[-1]       # target object (last selection)
+        result = stretchmodel(target, REFBOTTOM, REFTOP, VERTSTOP, self.desired_height)
+        return result
+
+    def invoke(self, context, event):
+        wm = context.window_manager
+        return wm.invoke_props_dialog(self)
+
+
+bpy.utils.register_class(AskSizeDialogOperator)
+
+#   Call this to use.
+def linearstretch() :
+    bpy.ops.object.ask_size_dialog_operator('INVOKE_DEFAULT')
+
+#   Test only
 def test() :
-    msg = stretchmodel(REFBOTTOM, REFTOP, VERTSTOP, 5.0)
+    context = C
+    if not context.selected_objects :
+        return({'ERROR_INVALID_INPUT'}, "Nothing selected.")
+    target = context.selected_objects[-1]       # target impostor (last selection)
+
+    msg = stretchmodel(target, REFBOTTOM, REFTOP, VERTSTOP, 5.0)
     if (msg) :
         print("Error: %s" % (str(msg),))
